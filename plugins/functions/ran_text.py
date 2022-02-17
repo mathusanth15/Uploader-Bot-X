1b0c4efbfaafe39ab1a9b0aa25a2094bf827c232
TamilanXBots <92419707+MoneyEarnVip@users.noreply.github.com>
2022-02-17 18:13:39 +0530

    Create ran_text.py

---
+++ plugins/functions/ran_text.py
@@ -0,0 +1,9 @@
+# © @Disney_Bots | @MoneyEarnVip
+
+import random
+import string
+
+def random_char(y):
+       return ''.join(random.choice(string.ascii_letters) for x in range(y))
+
+ran = (random_char(5)) 
