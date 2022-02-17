cc7ef313c1c1998eae4ea5e545fee6020b80d2a3
TamilanXBots <92419707+MoneyEarnVip@users.noreply.github.com>
2022-02-17 19:36:08 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 echo "Cloning Repo...."
-git clone https://github.com/MoneyEarnVip/Uploader-Bot-X /Uploader-Bot-X
+git clone https://github.com/Tellybots/Uploader-Bot /Uploader-Bot
 cd /Uploader-Bot
 pip3 install -U -r requirements.txt
-echo "Starting Bot.... Please Wait. Check You Log. Bot Credit @DKBOTZ\n Special Credit :- @Disney_bots"
+echo "Starting Bot.... Please Wait. Check You Log. Bot Credit @DKBOTZ\n Special Credit :- @Tellybots"
 python3 bot.py
