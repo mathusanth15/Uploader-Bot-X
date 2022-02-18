2ca43b537e60c7dc5cfb584d6b2abd3337e80f0b
TamilanXBots <92419707+MoneyEarnVip@users.noreply.github.com>
2022-02-18 10:52:58 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -4,16 +4,16 @@
 
 import os
 from config import Config
-from pyrogram import Client as Tamilanxbots
+from pyrogram import Client as MoneyEarnVip
 
 if __name__ == "__main__" :
     # create download directory, if not exist
     if not os.path.isdir(Config.DOWNLOAD_LOCATION):
         os.makedirs(Config.DOWNLOAD_LOCATION)
     plugins = dict(root="plugins")
-    Tamilanxbots = Tamilanxbots("Uploader Bot X",
+    MoneyEarnVip = MoneyEarnVip("Uploader Bot X",
     bot_token=Config.BOT_TOKEN,
     api_id=Config.API_ID,
     api_hash=Config.API_HASH,
     plugins=plugins)
-    Tamilanxbots.run()
+    MoneyEarnVip.run()
