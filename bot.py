8cdbb68552c65d3b711cba04ac041933985799e8
TamilanXBots <92419707+MoneyEarnVip@users.noreply.github.com>
2022-02-18 13:06:50 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,19 +1,28 @@
-#!#!/usr/bin/env python3
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-# (c) Shrimadhav U K | Modifieded By : @DC4_WARRIOR
+
+import logging
+logging.basicConfig(level=logging.DEBUG,
+                    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+logger = logging.getLogger(__name__)
 
 import os
-from config import Config
-from pyrogram import Client as MoneyEarnVip
+from plugins.config import Config
+
+from pyrogram import Client as Tellybots
+from pyrogram import filters
+logging.getLogger("pyrogram").setLevel(logging.WARNING)
+
 
 if __name__ == "__main__" :
     # create download directory, if not exist
     if not os.path.isdir(Config.DOWNLOAD_LOCATION):
         os.makedirs(Config.DOWNLOAD_LOCATION)
     plugins = dict(root="plugins")
-    MoneyEarnVip = MoneyEarnVip("Uploader Bot X",
-    bot_token=Config.BOT_TOKEN,
-    api_id=Config.API_ID,
-    api_hash=Config.API_HASH,
-    plugins=plugins)
-    MoneyEarnVip.run()
+    Tellybots = Tellybots(
+        "Uploader Bot",
+        bot_token=Config.TG_BOT_TOKEN,
+        api_id=Config.APP_ID,
+        api_hash=Config.API_HASH,
+        plugins=plugins)
+    Tellybots.run()
